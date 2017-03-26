46fa238bdbb789526e49f25906610cbe32aea5b6
Martino <martino.tiziani@studenti.unimi.it>
2017-03-26 18:44:09 +0200

    risolto bug e aggiunta riduzione pivot

--- riduzione_gaussiana.c
+++ riduzione_gaussiana.c
@@ -8,7 +8,7 @@ int main (void)
                         2.0, 2.0, 3.0, 5.0};
 					  
 	int s,r,c,i,j,row,col,n,k;
-	double x,y,q;
+	double x,y,q,pivot,tmp;
 
 	row = 3;
 	col = 4;
@@ -17,20 +17,37 @@ int main (void)
 
 	for(j=0; j<n; j++) 
 		{
-			for(i=0; i<n; i++)
+			for(i=0; i<col-1; i++)
 			{
 				if(i>j)
 				{         
 					q = gsl_matrix_get(&m.matrix,i,j) / gsl_matrix_get(&m.matrix,j,j); //q=M[i][j]/M[j][j];
-					for(k=0; k<n+1; k++)
+					for(k=0; k<col; k++)
 					{
 						x = gsl_matrix_get(&m.matrix,i,k);
 						y = gsl_matrix_get(&m.matrix,j,k);
-						gsl_matrix_set (&m.matrix,i,k, x-(q*y));      //M[i][k]=M[i][k]-q*M[j][k];
+						tmp = q*y;
+						gsl_matrix_set (&m.matrix,i,k, x-tmp);      //M[i][k]=M[i][k]-q*M[j][k];
+		
 					}
 				}
 			}
 		}
+
+
+	//riduce i pivot a 1
+	for(i=0;i<n;i++){
+		j=0;
+		do{
+			pivot = gsl_matrix_get(&m.matrix,i,j);
+			j++;
+		}while(pivot == 0.0);
+		if(pivot != 1.0){
+			for(k=j-1;k<col;k++){
+				gsl_matrix_set (&m.matrix,i,k, gsl_matrix_get(&m.matrix,i,k)/pivot);			
+			}
+		}
+	}
 
 
 	printf("Matrice di Gauss\n");
@@ -40,9 +57,9 @@ int main (void)
 		{
 			if(j==col-1)
 			{
-				printf("| %f ",gsl_matrix_get(&m.matrix,i,j));						
+				printf("| %g ",gsl_matrix_get(&m.matrix,i,j));						
 			}else{
-				printf("%f ",gsl_matrix_get(&m.matrix,i,j));						
+				printf("%g ",gsl_matrix_get(&m.matrix,i,j));						
 			}	
 		}
 		printf("\n");
