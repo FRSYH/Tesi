604dbc0a543b14b986819559fe70d37c09a57bd7
Martino <martino.tiziani@studenti.unimi.it>
2017-03-28 15:40:13 +0200

    Aggiunte funzioni per riduzione di gauss e stampa matrice formattata

---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <gsl/gsl_linalg.h>
+
+void gauss_riduzione(gsl_matrix * m, int row, int col){
+
+	int i,j,k;
+	double x,y,q,pivot,tmp;
+
+	for(j=0; j<row;j++) 
+		{
+			for(i=0; i<col-1; i++)
+			{
+				if(i>j)
+				{         
+					q = gsl_matrix_get(m,i,j) / gsl_matrix_get(m,j,j); //q=M[i][j]/M[j][j];
+					for(k=0; k<col; k++)
+					{
+						x = gsl_matrix_get(m,i,k);
+						y = gsl_matrix_get(m,j,k);
+						tmp = q*y;
+						gsl_matrix_set (m,i,k, x-tmp);      //M[i][k]=M[i][k]-q*M[j][k];
+		
+					}
+				}
+			}
+		}
+
+
+	//riduce i pivot a 1
+	for(i=0;i<row;i++){
+		j=0;
+		do{
+			pivot = gsl_matrix_get(m,i,j);
+			j++;
+		}while(pivot == 0.0);
+		if(pivot != 1.0){
+			for(k=j-1;k<col;k++){
+				gsl_matrix_set (m,i,k, gsl_matrix_get(m,i,k)/pivot);			
+			}
+		}
+	}
+
+
+}
+
+
+
+void print_matrix (gsl_matrix * m, int row, int col){
+	
+	int i,j;	
+	
+	printf("Matrice di Gauss\n");
+	for (i=0;i<row;i++)
+	{
+		for (j=0;j<col;j++)
+		{
+			if(j==col-1)
+			{
+				printf("| %g ",gsl_matrix_get(m,i,j));						
+			}else{
+				printf("%g ",gsl_matrix_get(m,i,j));						
+			}	
+		}
+		printf("\n");
+	}
+	printf("\n");
+
+}
+
+
+int main (void)
+{
+	double a_data[] = { 1.0, 2.0, -1.0, 2.0, 
+                        4.0, 3.0, 1.0,  3.0,
+                        2.0, 2.0, 3.0, 5.0};
+					  	
+	int row,col;
+
+	row = 3;
+	col = 4;	
+
+	gsl_matrix_view m = gsl_matrix_view_array (a_data, row, col);
+
+	gauss_riduzione(&m.matrix, row, col);
+	
+	print_matrix(&m.matrix, row, col);
+
+	return 0;
+}
